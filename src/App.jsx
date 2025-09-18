a25c1d831fae06d6ab23ed61ccb5f00d8a15f0f3
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-09-18 19:47:10 -0400

    contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Home from './components/home/Home'
 import Contact from './components/contact/Contact'
+import ScrollToTop from './components/layout/ScrollTop'
 
 function App() {
 
   return (
     <div>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path='*' element={<Home />} />
